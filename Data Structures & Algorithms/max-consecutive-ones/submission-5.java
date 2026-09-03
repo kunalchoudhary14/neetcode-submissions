c576451fc1135f4b9a34770a70bbca4839013087
Kunal choudhary <103172911+kunalchoudhary14@users.noreply.github.com>
2026-09-04 02:38:48 +0530

    Add: max-consecutive-ones - submission-5

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n = nums.length;
+        int c=0;
+        int maxc=0;
+        for(int i:nums){
+            if(i==1) {
+                c++;
+                maxc= Math.max(maxc,c);
+            }
+            else c=0;
+            
+        }
+        
+        return maxc;
+    }
+}
